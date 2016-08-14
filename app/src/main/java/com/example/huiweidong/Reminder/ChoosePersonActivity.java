23685ad936863f51542d0dc7a0a6959f603674f0
Jörn Notbom <joern.notbom@gmail.com>
2016-08-14 11:49:06 +0200

    - Bugfix: App will now show phone contacts without crash - add Method which handles the klick on ListItem

--- app/src/main/java/com/example/huiweidong/Reminder/ChoosePersonActivity.java
+++ app/src/main/java/com/example/huiweidong/Reminder/ChoosePersonActivity.java
@@ -1,11 +1,12 @@
 package com.example.huiweidong.Reminder;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,7 +15,6 @@ import java.util.ArrayList;
 
 
 public class ChoosePersonActivity extends AppCompatActivity {
-    public Intent i = new Intent(this, MainActivity.class);
     ListView list;
     //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
     ArrayList<String> listItems=new ArrayList<String>();
@@ -37,9 +37,22 @@ public class ChoosePersonActivity extends AppCompatActivity {
                 listItems);
         //setListAdapter(adapter);
         list.setAdapter(adapter);
+
+
+        // This routine handles the "which list Item is clicked" problem
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapter, View myView, int myItemInt, long mylng) {
+                String selectedFromList = (String) (list.getItemAtPosition(myItemInt));
+                Log.i("DEBUG", selectedFromList);
+
+            }
+        });
         getPersonsFromContacts();
     }
 
+    /**
+     * gets the contacts
+     */
     public void getPersonsFromContacts() {
         // Do something in response to button
         // get the whole table
@@ -64,8 +77,13 @@ public class ChoosePersonActivity extends AppCompatActivity {
         c.close();
     }
 
+    /**
+     * @param id
+     * @return
+     */
    public String getPhoneNumber(String id)
     {
+        // TODO: Bugfixing! Does not as expectedt. some Phonenumbers are not showed
         String number = "";
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone._ID + " = " + id, null, null);
 
